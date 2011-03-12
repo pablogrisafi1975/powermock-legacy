648b6b69f00213bc801af119e825c06cec3e1465
pablogrisafi1975@gmail.com <pablogrisafi1975@gmail.com@10fb77df-9b1e-aae5-d1cb-fe90db3dd954>
2011-03-12 19:01:00 +0000

    Fix: It didn't worked in Java 1.4, because it tried to create an instance of TestCase with no real need, and TestCase does not have a no arguments constructor

--- trunk/PowerMock-Legacy/Retrotranslator-1.2.9-bin/RetrotranslatePowerMock-Legacy.bat
+++ trunk/PowerMock-Legacy/Retrotranslator-1.2.9-bin/RetrotranslatePowerMock-Legacy.bat
@@ -3,7 +3,7 @@
 ::Next version maybe.
 
 ::Assuming this is the right folder
-cd C:\Users\Pablo\workspace\PowerMock-Legacy\Retrotranslator-1.2.9-bin
+cd C:\Users\Pablo\workspace\PowerMock-Legacy-01\Retrotranslator-1.2.9-bin
 
 ::Assuming the jar file is exported as c:\temp\PowerMock-Legacy.jar
 java  -jar retrotranslator-transformer-1.2.9.jar -srcjar c:\temp\PowerMock-Legacy.jar -destjar c:\temp\PowerMock-Legacy-jvm14-13.0.1.jar  -target 1.4
