b3e251912e3a97155f0c81716e2f9295dd28a1bc
pablogrisafi1975@gmail.com <pablogrisafi1975@gmail.com@10fb77df-9b1e-aae5-d1cb-fe90db3dd954>
2011-03-19 14:20:40 +0000

    Warnings removal Interface IPowerMockIgnore works New test to check it

--- trunk/PowerMock-Legacy/src/org/powermock/tests/utils/impl/PowerMockIgnorePackagesExtractorImpl.java
+++ trunk/PowerMock-Legacy/src/org/powermock/tests/utils/impl/PowerMockIgnorePackagesExtractorImpl.java
@@ -5,6 +5,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.core.classloader.interfaces.IPowerMockIgnore;
 import org.powermock.tests.utils.IgnorePackagesExtractor;
 
 public class PowerMockIgnorePackagesExtractorImpl implements IgnorePackagesExtractor {
@@ -18,6 +19,25 @@ public class PowerMockIgnorePackagesExtractorImpl implements IgnorePackagesExtra
                 ignoredPackages.add(ignorePackage);
             }
         }
+        //Interface based for PowerMockLegacy
+        if (element instanceof Class<?>) {
+        	Class<?> clazz = (Class<?>) element;
+        	if(IPowerMockIgnore.class.isAssignableFrom(clazz)){
+        		try {
+					Object newInstance = clazz.newInstance();
+					IPowerMockIgnore iPowerMockIgnore = (IPowerMockIgnore) newInstance;
+					String[] ignores = iPowerMockIgnore.fullyQualifiedNamesToIgnore();
+		            for (String ignorePackage : ignores) {
+		                ignoredPackages.add(ignorePackage);
+		            }
+				} catch (InstantiationException e) {
+					e.printStackTrace();
+				} catch (IllegalAccessException e) {
+					e.printStackTrace();
+				}
+        	}        
+        }
+        
         if (element instanceof Class<?>) {
             Class<?> klazz = (Class<?>) element;
             Class<?> superclass = klazz.getSuperclass();
