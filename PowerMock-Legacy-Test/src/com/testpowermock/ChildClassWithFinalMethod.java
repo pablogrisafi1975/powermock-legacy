ad666fcb9bf539a8ea59605ff75a092e8f73d534
pablogrisafi1975@gmail.com <pablogrisafi1975@gmail.com@10fb77df-9b1e-aae5-d1cb-fe90db3dd954>
2011-03-19 14:27:28 +0000

    Interfaces working: IPrepareForTest, IPrepareOnlyThisForTest, IPrepareEverythingForTest, IPowerMockIgnore

--- PowerMock-Legacy-Test/src/com/testpowermock/ChildClassWithFinalMethod.java
+++
@@ -1,8 +0,0 @@
-package com.testpowermock;
-
-public class ChildClassWithFinalMethod extends ClassWithFinalMethod {
-	public final String getStringInChild(){
-		return "ORIGINAL_STRING_IN_CHILD";
-	}
-
-}
--- PowerMock-Legacy-Test/src/com/testpowermock/ClassWithFinalMethod.java
+++
@@ -1,7 +0,0 @@
-package com.testpowermock;
-
-public class ClassWithFinalMethod {
-	public final String getString() {
-		return "ORIGINAL_STRING";
-	}
-}
